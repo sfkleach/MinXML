ebd25fecdf572191569eb39029e797935dc150f7
Stephen Leach <sfkleach@gmail.com>
2014-10-22 08:43:22 +0100

    Utility classes.

---
+++ src/com/steelypip/powerups/common/EmptySet.java
@@ -0,0 +1,37 @@
+package com.steelypip.powerups.common;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.AbstractSet;
+
+public class EmptySet< T > extends AbstractSet< T > {
+	
+	static Iterator< Object > empty_iterator = new EmptyIterator< Object >();
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Iterator< T > iterator() {
+		return ( Iterator< T > )empty_iterator;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return true;
+	}
+
+	@Override
+	public boolean contains( Object o ) {
+		return false;
+	}
+
+	@Override
+	public boolean containsAll( Collection< ? > c ) {
+		return false;
+	}	
+
+}

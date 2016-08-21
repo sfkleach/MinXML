c56cb42becbe99dfdc3f9e5085f05ec70e9e2e91
Stephen Leach <sfkleach@gmail.com>
2016-08-21 16:12:58 +0100

    Some javadoc added.

--- src/com/steelypip/powerups/hydra/ImmutableHydra.java
+++ src/com/steelypip/powerups/hydra/ImmutableHydra.java
@@ -1,5 +1,8 @@
 package com.steelypip.powerups.hydra;
 
+/**
+ * A marker interface to show that a class is immutable.
+ */
 public interface ImmutableHydra {
 
 }
--- src/com/steelypip/powerups/hydra/MutableHydra.java
+++ src/com/steelypip/powerups/hydra/MutableHydra.java
@@ -1,5 +1,8 @@
 package com.steelypip.powerups.hydra;
 
+/**
+ * A marker interface to show that a class is mutable.
+ */
 public interface MutableHydra {
 
 }

085833afe0e1e089625eb9c15069b32b9ec19683
Stephen Leach <sfkleach@gmail.com>
2016-08-30 09:07:41 +0100

    Switch in the new implementations

--- src/com/steelypip/powerups/fusion/FusionSearcher2.java
+++
@@ -1,8 +0,0 @@
-package com.steelypip.powerups.fusion;
-
-import com.steelypip.powerups.hydraxml.AbsMultiTreeSearcher;
-
-
-public abstract class FusionSearcher2 extends AbsMultiTreeSearcher< String, Fusion > implements FusionAsMultiTree {
-	//	Skip.
-}
--- src/com/steelypip/powerups/fusion/FusionWalker2.java
+++
@@ -1,7 +0,0 @@
-package com.steelypip.powerups.fusion;
-
-import com.steelypip.powerups.hydraxml.AbsMultiTreeWalker;
-
-public abstract class FusionWalker2 extends AbsMultiTreeWalker< String, Fusion > implements FusionAsMultiTree {
-	//	Skip.
-}
